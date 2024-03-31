863a564407fc0458fb229735d1f30bc4c237944a
StanaAndrei <stadey33@gmail.com>
2024-03-31 17:00:29 +0300

    set pallete

--- eobfe/src/renderer/src/App.tsx
+++ eobfe/src/renderer/src/App.tsx
@@ -1,25 +1,23 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './components/home/Home'
-import { RouterProvider, createHashRouter } from "react-router-dom"
+import { RouterProvider, createHashRouter } from 'react-router-dom'
 
 const router = createHashRouter([
   {
-    path: "/",
+    path: '/',
     element: (
       <>
         <Home />
       </>
-    ),
-  },
+    )
+  }
 ])
-
+//pallete: https://coolors.co/palette/0081a7-00afb9-fdfcdc-fed9b7-f07167
 function App(): JSX.Element {
   return (
     <>
-    <div>123</div>
+      <div>123</div>
       <RouterProvider router={router} />
     </>
-    
   )
 }
 
